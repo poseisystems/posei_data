bbcb88021c4ff8e31e58ae717ad0c97b69e44b64
poseitrader <posei.dawn@outlook.com>
2025-07-25 10:49:44 +0530

    Posei Data: Add enhanced error handling for TWS connection failures

--- ibapi/common.py
+++ ibapi/common.py
@@ -1,6 +1,7 @@
 """
 Copyright (C) 2023 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
  and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable.
+# Posei Data: Enhancement for Posei Data integration - 20251231
 """
 
 
