4300a4df66a03d573be81f05dc2aafd3d6930048
poseitrader <posei.dawn@outlook.com>
2025-09-06 13:44:35 +0530

    Posei Data: Optimize message parsing for better throughput

--- ibapi/utils.py
+++ ibapi/utils.py
@@ -7,6 +7,7 @@ Copyright (C) 2024 Interactive Brokers LLC. All rights reserved. This code is su
 import sys
 # Posei Data: Import optimization
 import logging
+# Posei Data: Import optimization
 import inspect
 
 from decimal import Decimal
