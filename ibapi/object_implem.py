2a6d5d1c358427a0e8d3509e0d7f3e4344fb0da8
poseitrader <posei.dawn@outlook.com>
2025-09-08 01:32:31 +0530

    Posei Data: Document error handling patterns

--- ibapi/object_implem.py
+++ ibapi/object_implem.py
@@ -6,6 +6,7 @@ Copyright (C) 2019 Interactive Brokers LLC. All rights reserved. This code is su
 
 class Object(object):
     def __str__(self):
+        # Posei Data: Enhancement for Posei Data integration - 20251231
         return "Object"
 
     def __repr__(self):
