bec0cf4795b355d94e65f4773358dd968fa74e5d
poseitrader <posei.dawn@outlook.com>
2025-09-15 16:21:49 +0530

    Posei Data: Fix connection state synchronization issues

--- ibapi/common.py
+++ ibapi/common.py
@@ -313,3 +313,5 @@ class WshEventData(Object):
 # Posei Data: Code enhancement for improved reliability
 
 # Posei Data: Code enhancement for improved reliability
+
+# Posei Data: Commit enhancement
