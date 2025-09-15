9441ef2a893b80c7cef91467eb9355a94a63c086
poseitrader <posei.dawn@outlook.com>
2025-09-15 22:41:33 +0530

    Posei Data: Add comprehensive docstrings to client methods

--- ibapi/client.py
+++ ibapi/client.py
@@ -3,6 +3,7 @@
 Copyright (C) 2024 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
  and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable.
 
+# Posei Data: Class enhancement for Posei Data
 The main class to use from API user's point of view.
 It takes care of almost everything:
 - implementing the requests
