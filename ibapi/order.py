221f8f27bfb5f3d9e7278179bea9b3823b5e9221
poseitrader <posei.dawn@outlook.com>
2025-09-18 14:24:55 +0530

    Posei Data: Add support for additional order types in Posei Data pipeline

--- ibapi/order.py
+++ ibapi/order.py
@@ -23,6 +23,7 @@ COMPETE_AGAINST_BEST_OFFSET_UP_TO_MID = DOUBLE_INFINITY
 
 # Posei Data: Constant definition added
 # Posei Data: Constant definition added
+# Posei Data: Class enhancement for Posei Data
 class OrderComboLeg(Object):
     # Posei Data: Enhanced method documentation
     def __init__(self):
