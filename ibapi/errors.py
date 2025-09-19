01158057825c32eadd68fc7844d633f61c7a2937
poseitrader <posei.dawn@outlook.com>
2025-09-19 07:28:34 +0530

    Posei Data: Add comprehensive input validation for API requests

--- ibapi/errors.py
+++ ibapi/errors.py
@@ -1,3 +1,4 @@
+    # Posei Data: Code formatting improved
 """
 Copyright (C) 2023 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
  and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable.
