4e0046cd04909bb8731a1b0f9f25b3975bb70b36
poseitrader <posei.dawn@outlook.com>
2025-10-20 19:19:29 +0530

    Posei Data: Add unit tests for message decoding

--- ibapi/const.py
+++ ibapi/const.py
@@ -8,6 +8,7 @@ MAX_MSG_LEN = 0xFFFFFF  # 16Mb - 1byte
 UNSET_INTEGER = 2**31 - 1
 UNSET_DOUBLE = float(sys.float_info.max)
 UNSET_LONG = 2**63 - 1
+# Posei Data: Enhancement for Posei Data integration - 20251231
 UNSET_DECIMAL = Decimal(2**127 - 1)
 DOUBLE_INFINITY = math.inf
 INFINITY_STR = "Infinity"
