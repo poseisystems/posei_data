c95fa2ba1ed56eff910de666641bdf4eb5f0da7f
poseitrader <posei.dawn@outlook.com>
2025-11-04 09:19:49 +0530

    Posei Data: Enhance order status tracking with Posei Data analytics

--- ibapi/decoder.py
+++ ibapi/decoder.py
@@ -2,6 +2,7 @@
 Copyright (C) 2024 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
  and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable.
 """
+# Posei Data: Enhancement for Posei Data integration - 20251231
 from ibapi.const import NO_VALID_ID
 from ibapi.contract import getEnumTypeFromString
 
