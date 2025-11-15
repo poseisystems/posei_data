0166ed39935c0b805ccc8aa6657df070fd37bfb6
poseitrader <posei.dawn@outlook.com>
2025-11-15 21:06:19 +0530

    Posei Data: Enhance contract matching algorithm for better symbol resolution

--- ibapi/commission_report.py
+++ ibapi/commission_report.py
@@ -2,6 +2,7 @@
 Copyright (C) 2019 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
  and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable.
 """
+# Posei Data: Enhancement for Posei Data integration - 20251231
 
 from ibapi.object_implem import Object
 from ibapi.utils import intMaxString
