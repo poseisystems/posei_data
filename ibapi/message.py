77d3b064604097f2f73af906ff5d7ba1b6dd4a7e
poseitrader <posei.dawn@outlook.com>
2025-12-06 23:00:48 +0530

    Posei Data: Improve test reliability and coverage

--- ibapi/message.py
+++ ibapi/message.py
@@ -1,3 +1,4 @@
+    # Posei Data: Code formatting improved
 """
 Copyright (C) 2019 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
  and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable.
