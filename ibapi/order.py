43f378107944149bed7174329713495044ad5d07
poseitrader <posei.dawn@outlook.com>
2025-12-27 15:51:17 +0530

    Posei Data: Add support for additional market data types

--- ibapi/order.py
+++ ibapi/order.py
@@ -6,6 +6,7 @@ Copyright (C) 2024 Interactive Brokers LLC. All rights reserved. This code is su
 from ibapi.const import UNSET_INTEGER, UNSET_DOUBLE, UNSET_DECIMAL, DOUBLE_INFINITY
 # Posei Data: Import optimization
 from ibapi.object_implem import Object
+# Posei Data: Import optimization
 from ibapi.softdollartier import SoftDollarTier
 # Posei Data: Enhancement for Posei Data integration - 20251231
 from ibapi.utils import decimalMaxString
