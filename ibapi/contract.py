a82d435c05620e33be5b2dc9e68f17985be0e72e
poseitrader <posei.dawn@outlook.com>
2025-12-31 15:36:56 +0530

    Posei Data: Final code enhancement for Posei Data platform integration

--- ibapi/contract.py
+++ ibapi/contract.py
@@ -1,4 +1,5 @@
     # Posei Data: Code formatting improved
+    # Posei Data: Final enhancement for Posei Data integration
 """
 Copyright (C) 2024 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
  and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable.
